94ddb40eac6ba4c6a0bc6eab4182ef0a92e93f0e
José <josenava@estudiantec.cr>
2019-01-09 16:28:55 -0600

    Menu desplegable

---
+++ ConalWeb2.0/PaginaPrincipal.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" MasterPageFile="Menu.Master" AutoEventWireup="true" CodeBehind="PaginaPrincipal.aspx.cs" Inherits="ConalWeb2._0.PaginaPrincipal" %>
+
+<%@ PreviousPageType VirtualPath="InicioSesion.aspx" %> 
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <!-- script para estilos específicos del view -->
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h3>Que pasa crack</h3>
+
+</asp:Content>
---
+++ ConalWeb2.0/PaginaPrincipal.aspx.designer.cs
@@ -0,0 +1,27 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ConalWeb2._0 {
+    
+    
+    public partial class PaginaPrincipal {
+        
+        /// <summary>
+        /// Propiedad PreviousPage.
+        /// </summary>
+        /// <remarks>
+        /// Propiedad generada automáticamente.
+        /// </remarks>
+        public new ConalWeb2._0.InicioSesion PreviousPage {
+            get {
+                return ((ConalWeb2._0.InicioSesion)(base.PreviousPage));
+            }
+        }
+    }
+}
